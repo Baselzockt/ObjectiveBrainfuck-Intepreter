f93163eecf5080a3e3bed40b077e0ccdc19d8d85
Baselzockt <fabiokaufmann10@gmail.com>
2020-07-28 12:52:49 +0200

    relocated plugin Handling to the Interpreter.

--- src/com/company/Interpreter.java
+++ src/com/company/Interpreter.java
@@ -2,11 +2,14 @@ package com.company;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.LinkedList;
+import java.util.Properties;
+import java.util.jar.JarFile;
 
 public class Interpreter {
     private InputStream in;
@@ -16,6 +19,9 @@ public class Interpreter {
     private static final int MEMORYSIZE = 32768;
     private LinkedList<Integer> openedLoops;
     private BFO bfo;
+    private HashSet<IPlugin> plugins;
+    private Properties properties;
+    private static String configFile = "app.config";
 
     public InputStream getIn() {
         return in;
@@ -65,23 +71,36 @@ public class Interpreter {
         this.bfo = bfo;
     }
 
-    public Interpreter(InputStream input, OutputStream output, String[] definitions, HashSet<IPlugin> plugins){
-        this(input,output,MEMORYSIZE,definitions,plugins);
+    public Interpreter(InputStream input, OutputStream output, String[] definitions){
+        this(input,output,MEMORYSIZE,definitions);
     }
 
-    private HashSet<IPlugin> plugins;
 
     public HashSet<IPlugin> getPlugins() {
         return plugins;
     }
 
-    public Interpreter(InputStream input, OutputStream output, int memorySize, String[] definitions, HashSet<IPlugin> plugins){
+    public Interpreter(InputStream input, OutputStream output, int memorySize, String[] definitions,HashSet<IPlugin> plugins) {
         this.in = input;
         this.out = output;
         this.fields = new int[memorySize];
         this.openedLoops = new LinkedList<>();
         this.objectDefinitions = definitions;
         this.plugins = plugins;
+    }
+
+    public Interpreter(InputStream input, OutputStream output, int memorySize, String[] definitions){
+        try {
+            loadProperties();
+            loadPlugins();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        this.in = input;
+        this.out = output;
+        this.fields = new int[memorySize];
+        this.openedLoops = new LinkedList<>();
+        this.objectDefinitions = definitions;
     }
     private int pointer;
 
@@ -141,6 +160,61 @@ public class Interpreter {
         tmpBfo.setPointer(0);
 
         return tmpBfo;
+    }
+
+    private void loadProperties() throws IOException {
+        properties = new Properties();
+        File file = new File(configFile);
+        if(!file.exists()){
+            FileWriter writer = new FileWriter(file);
+            writer.write("PLUGIN_DIRECTORY=plugin");
+            writer.close();
+        }
+        properties.load(new FileInputStream(file));
+        System.out.println("Loaded Properties");
+    }
+
+    private void loadPlugins() throws IOException {
+        plugins= new HashSet<>();
+        File pluginDirectory = new File(properties.getProperty("PLUGIN_DIRECTORY"));
+        if(!pluginDirectory.exists())pluginDirectory.mkdir();
+        File[] files = pluginDirectory.listFiles((dir,name)-> name.endsWith(".jar"));
+
+        if(files!=null && files.length > 0){
+            ArrayList<String> classes=new ArrayList<>();
+            ArrayList<URL> urls=new ArrayList<>(files.length);
+            for(File file:files)
+            {
+                JarFile jar=new JarFile(file);
+                jar.stream().forEach(jarEntry -> {
+                    if(jarEntry.getName().endsWith(".class"))
+                    {
+                        classes.add(jarEntry.getName());
+                    }
+                });
+                URL url=file.toURI().toURL();
+                urls.add(url);
+            }
+            URLClassLoader urlClassLoader=new URLClassLoader(urls.toArray(new URL[urls.size()]));
+            classes.forEach(className->{
+                try
+                {
+                    Class cls=urlClassLoader.loadClass(className.replaceAll("/",".").replace(".class","")); //transforming to binary name
+                    Class[] interfaces=cls.getInterfaces();
+                    for(Class intface:interfaces)
+                    {
+                        if(intface.equals(IPlugin.class)) //checking presence of Plugin interface
+                        {
+                            IPlugin IPlugin =(IPlugin) cls.newInstance(); //instantiating the Plugin
+                            plugins.add(IPlugin);
+                            break;
+                        }
+                    }
+                }
+                catch (Exception e){e.printStackTrace();}
+            });
+            plugins.forEach(IPlugin::initialize);
+        }
     }
 
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,93 +1,12 @@
 package com.company;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.net.URL;
-import java.net.URLClassLoader;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Properties;
-import java.util.jar.JarFile;
-
 public class Main {
 
-    private HashSet<IPlugin> plugins;
-    private Properties properties;
-    private static String configFile = "app.config";
-
-    public static void main(String[] args) throws IOException {
-       Main main = new Main();
-    }
-    Main() throws IOException {
-        try {
-            loadProperties();
-            loadPlugins();
-        }catch(Exception e){
-            e.printStackTrace();
-        }
-        String[] definitions = new String[]{"¦>?{1}{*0}{*1}¦","¦$>++++++++[-<+++++++++>]<.>>+>-[+]++>++>+++[>[->+++<<+++>]<<]>-----.>->+++..+++.>-.<<+[>[+>+]>>]<--------------.>>.+++.------.--------.>+.>+.¦ ¦++..¦"};
-        Interpreter ip = new Interpreter(System.in,System.out,definitions,plugins);
+    public static void main(String[] args)  {
+        String[] definitions = new String[]{"¦>?{1}{*0}{*1}¦", "¦$>++++++++[-<+++++++++>]<.>>+>-[+]++>++>+++[>[->+++<<+++>]<<]>-----.>->+++..+++.>-.<<+[>[+>+]>>]<--------------.>>.+++.------.--------.>+.>+.¦ ¦++..¦"};
+        Interpreter ip = new Interpreter(System.in, System.out, definitions);
         ip.run();
         System.out.flush();
     }
 
-    private void loadProperties() throws IOException {
-        properties = new Properties();
-        File file = new File(configFile);
-        if(!file.exists()){
-            FileWriter writer = new FileWriter(file);
-            writer.write("PLUGIN_DIRECTORY=plugin");
-            writer.close();
-        }
-        properties.load(new FileInputStream(file));
-        System.out.println("Loaded Properties");
-    }
-
-    private void loadPlugins() throws IOException {
-         plugins= new HashSet<>();
-        File pluginDirectory = new File(properties.getProperty("PLUGIN_DIRECTORY"));
-        if(!pluginDirectory.exists())pluginDirectory.mkdir();
-        File[] files = pluginDirectory.listFiles((dir,name)-> name.endsWith(".jar"));
-
-        if(files!=null && files.length > 0){
-            ArrayList<String> classes=new ArrayList<>();
-            ArrayList<URL> urls=new ArrayList<>(files.length);
-            for(File file:files)
-            {
-                JarFile jar=new JarFile(file);
-                jar.stream().forEach(jarEntry -> {
-                    if(jarEntry.getName().endsWith(".class"))
-                    {
-                        classes.add(jarEntry.getName());
-                    }
-                });
-                URL url=file.toURI().toURL();
-                urls.add(url);
-            }
-
-            URLClassLoader urlClassLoader=new URLClassLoader(urls.toArray(new URL[urls.size()]));
-            classes.forEach(className->{
-                try
-                {
-                    Class cls=urlClassLoader.loadClass(className.replaceAll("/",".").replace(".class","")); //transforming to binary name
-                    Class[] interfaces=cls.getInterfaces();
-                    for(Class intface:interfaces)
-                    {
-                        if(intface.equals(IPlugin.class)) //checking presence of Plugin interface
-                        {
-                            IPlugin IPlugin =(IPlugin) cls.newInstance(); //instantiating the Plugin
-                            plugins.add(IPlugin);
-                            break;
-                        }
-                    }
-                }
-                catch (Exception e){e.printStackTrace();}
-            });
-
-
-            plugins.forEach(IPlugin::initialize);
-        }
-    }
 }
