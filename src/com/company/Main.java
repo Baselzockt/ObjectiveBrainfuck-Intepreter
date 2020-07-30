cd8918711bb589f1631b2885b3067e50d2b5ee39
Baselzockt <fabiokaufmann10@gmail.com>
2020-07-30 11:22:53 +0200

    added .idea to git ignore

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args)  {
-        String[] definitions = new String[]{"&¦>?{1}{*0}{*1}¦&", "&¦$>++++++++[-<+++++++++>]<.>>+>-[+]++>++>+++[>[->+++<<+++>]<<]>-----.>->+++..+++.>-.<<+[>[+>+]>>]<--------------.>>.+++.------.--------.>+.>+.¦ ¦++..¦&"};
+        String[] definitions = new String[]{"¦+++?{1}[{0}{*1}(2000)>+<-]¦",
+                "¦$@>++++++++[-<+++++++++>]<.>>+>-[+]++>++>+++[>[->+++<<+++>]<<]>-----.>->+++..+++.>-.<<+[>[+>+]>>]<--------------.>>.+++.------.--------.>+.>+.¦ ¦++..¦ ¦$@+[,.]¦"};
         Interpreter ip = new Interpreter(System.in, System.out, definitions);
         ip.run();
         System.out.flush();
